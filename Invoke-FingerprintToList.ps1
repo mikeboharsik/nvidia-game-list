67c12f8fa9e2f08f60b2a7295395fa2d2df5026b
Mike Boharsik <mboharsik@gmail.com>
2023-03-12 00:43:38 -0500

    init

---
+++ Invoke-FingerprintToList.ps1
@@ -0,0 +1,30 @@
+$fingerprintPath = "$ENV:LOCALAPPDATA\NVIDIA\NvBackend\ApplicationOntology\data\fingerprint.db"
+
+$databaseInfo = Get-ChildItem $fingerprintPath
+
+$processedData = Select-Xml -Path $fingerprintPath -Xpath '//Fingerprint'
+	| ForEach-Object {
+		$node = $_.node
+
+		$versions = $node.version | ForEach-Object {
+			return [ordered]@{
+				name = $_.name
+				files = $_.files.file | ForEach-Object { $_.name }
+			}
+		}
+
+		return [ordered]@{
+			displayName = $node.displayName
+			name = $node.name
+			versions = $versions
+		}
+	} | Sort-Object { $_.displayName }
+
+$driverVersion = Get-WmiObject Win32_VideoController | Select-Object -ExpandProperty DriverVersion
+
+[ordered]@{
+	databaseLastWriteTimeUtc = $databaseInfo.LastWriteTimeUtc
+	driverVersion = $driverVersion
+	games = $processedData
+}	| ConvertTo-Json -Depth 10
+	| Set-Content "games.json"
